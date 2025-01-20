cce2b2973fd43921b67be523854cd2628c47c716
dev-ahad-ali <ahadali.dev@gmail.com>
2025-01-20 21:55:26 +0600

    Update: get conversation method added for modal

--- src/components/inbox/Modal.jsx
+++ src/components/inbox/Modal.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import { isValidEmail } from '../../utils/isValidEmail.js';
 import { useGetUsersQuery } from '../../features/users/usersApi.js';
 import Error from '../ui/Error.jsx';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { conversationsApi } from '../../features/conversations/conversationsApi.js';
 
 export default function Modal({ open, control }) {
   const [to, setTo] = useState('');
@@ -10,6 +14,27 @@ export default function Modal({ open, control }) {
   const { data: participant } = useGetUsersQuery(to, {
     skip: !userCheck,
   });
+  const { user: loggedInUser } = useSelector((state) => state.auth) || {};
+  const { email: myEmail } = loggedInUser || {};
+  const dispatch = useDispatch();
+  const [responseError, setResponseError] = useState('');
+  const [conversation, setConversation] = useState(undefined);
+
+  useEffect(() => {
+    if (participant?.length > 0 && participant[0].email !== myEmail) {
+      dispatch(
+        conversationsApi.endpoints.getConversation.initiate({
+          userEmail: myEmail,
+          participantEmail: to,
+        })
+      )
+        .unwrap()
+        .then((data) => {
+          setConversation(data);
+        })
+        .catch(() => setResponseError('There was a problem'));
+    }
+  }, [participant, myEmail, to, dispatch]);
 
   const debounceHandle = (fn, delay) => {
     let timeoutId;
@@ -76,13 +101,21 @@ export default function Modal({ open, control }) {
             <div>
               <button
                 type='submit'
-                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
+                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500 disabled:bg-gray-400'
+                disabled={
+                  conversation === undefined ||
+                  (participant?.length > 0 && participant[0].email === myEmail)
+                }
               >
                 Send Message
               </button>
             </div>
 
             {participant?.length === 0 && <Error message="This user doesn't exists" />}
+            {participant?.length > 0 && participant[0].email === myEmail && (
+              <Error message='You can not send message to yourself' />
+            )}
+            {responseError && <Error message={responseError} />}
           </form>
         </div>
       </>
