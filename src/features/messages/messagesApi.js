9c369601ced331547cc3d29355480104b22d65ab
dev-ahad-ali <ahadali.dev@gmail.com>
2025-01-20 13:54:15 +0600

    Update: messages added

--- src/features/messages/messagesApi.js
+++ src/features/messages/messagesApi.js
@@ -1,7 +1,14 @@
 import { apiSlice } from '../api/apiSlice.js';
 
 export const messagesApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => {
-    // inject endpoints
-  },
+  endpoints: (builder) => ({
+    getMessages: builder.query({
+      query: (id) =>
+        `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${
+          import.meta.env.VITE_MESSAGES_LIMIT
+        }`,
+    }),
+  }),
 });
+
+export const { useGetMessagesQuery } = messagesApi;
